23c0f1ee52e1b0a87b6744d43f03e4f53d31359f
Andrew Wanex <awanex2705@gmail.com>
2018-04-07 13:40:55 -0700

    added authentication handling

---
+++ authenticate.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken');
+
+const User = require('./models/user');
+const TOKEN_KEY = process.env.TOKEN_KEY;
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+exports.getToken = user => {
+  return jwt.sign(user, TOKEN_KEY, {expiresIn: '1d'});
+};
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = TOKEN_KEY;
+
+exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+  console.log("JWT payload", jwt_payload);
+  User.findOne({_id: jwt_payload.id}, (err, user) => {
+    if (err) {
+      return done(err, false);
+    } else if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  });
+}));
+
+exports.verifyUser = passport.authenticate('jwt', {session: false});
--- routes/user.js
+++ routes/user.js
@@ -39,4 +39,31 @@ userRouter.route('/login')
     })(req, res, next);
   });
 
+userRouter.route('/signup')
+  .post((req, res, next) => {
+    User.register(new User({username: req.body.username}),
+      req.body.password,
+      (err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+        } else {
+          user.save((err, user) => {
+            if (err) {
+              res.statusCode = 500;
+              res.setHeader('Content-Type', 'application/json');
+              res.json({err: err});
+              return;
+            }
+            passport.authenticate('local')(req, res, () => {
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'application/json');
+              res.json({success: true, status: 'Registration Successful'});
+            });
+          });
+        }
+      });
+  });
+
 module.exports = userRouter;

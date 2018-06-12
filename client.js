01b6624e4a681218bb4e35f150f4e6079f88e59f
Andrew Wanex <awanex2705@gmail.com>
2018-06-12 15:29:27 -0700

    refactor: set uri with username and password to cloud server address

--- client.js
+++ client.js
@@ -5,18 +5,17 @@ const https = require('https');
 const rp = require('request-promise');
 const socketClient = require('socket.io-client');
 const LocalStorage = require('node-localstorage').LocalStorage;
-const localStorage = new LocalStorage('./cache');
+const localStorage = new LocalStorage('./local-storage');
 
 
 exports.getClientToken = () => {
   const postData = {
     "username": `${process.env.SERVER_USERNAME}`,
-    "password": `${process.env.SERVER_PASSWORD}`,
-    "token": "test_token"
+    "password": `${process.env.SERVER_PASSWORD}`
   };
 
   const postOptions = {
-    uri: "http://httpbin.org/post",
+    uri: `${process.env.HOSTNAME}/${process.env.API_VERSION}/users/login`,
     method: 'POST',
     json: true,
     body: postData,

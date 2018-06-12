7153150128ca09713c38b01baf5025693a02adea
Andrew Wanex <awanex2705@gmail.com>
2018-06-12 15:28:21 -0700

    refactor: fix jwt payload id attribute to _id

--- authenticate.js
+++ authenticate.js
@@ -23,7 +23,7 @@ opts.secretOrKey = TOKEN_KEY;
 
 exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
   console.log("JWT payload", jwt_payload);
-  User.findOne({_id: jwt_payload.id}, (err, user) => {
+  User.findOne({_id: jwt_payload._id}, (err, user) => {
     if (err) {
       return done(err, false);
     } else if (user) {
